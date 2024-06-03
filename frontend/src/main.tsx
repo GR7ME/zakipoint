fcf5396d9dcc5a78a0d24f014e6876e04a36732f
gr7me <lsinjali40@gmail.com>
2024-06-03 15:22:42 +0545

    feat: signin page

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,27 +1,33 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import App from './App.tsx'
-import './index.css'
-import SignUp from './pages/login/SignUp.tsx';
-
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App.tsx";
+import "./index.css";
+import SignUp from "@/pages/login/SignUp.tsx";
+import SignIn from "@/pages/login/SignIn.tsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App/>,
+    element: <App />,
   },
   {
-    path: "/login",
-    element: <SignUp/>
+    path: "/login/",
+    children: [
+      {
+        path: "signup/",
+        element: <SignUp />,
+      },
+      {
+        path: "signin/",
+        element: <SignIn />,
+      },
+    ],
   }
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
---
+++ frontend/src/pages/login/SignIn.tsx
@@ -0,0 +1,75 @@
+import * as React from "react";
+import { useForm } from "react-hook-form";
+import { Button } from "@/components/ui/button";
+import { object, string, InferType } from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
+const authschema = object({
+  email: string()
+    .email("Invalid email format.")
+    .required("Email is required."),
+  password: string()
+    .required("Password is required.")
+})
+
+
+type InputType = InferType<typeof authschema>;
+
+const SignIn: React.FC = (): React.ReactElement => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<InputType>({
+    resolver: yupResolver(authschema),
+  });
+  const onSubmit = (data: InputType) => console.log(data);
+
+  console.log(watch("email"));
+
+  return (
+    <Card className="w-80 md:w-96">
+      <CardHeader>
+        <CardTitle>Login</CardTitle>
+        <CardDescription>Enter your email and password</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="grid w-full items-center gap-4">
+            <div className="flex flex-col space-y-1.5">
+              <Label htmlFor="name">Email</Label>
+              <Input id="name" placeholder="Email" {...register("email")} />
+              {errors.email && (
+                <Label className="text-destructive">
+                  {errors.email?.message}
+                </Label>
+              )}
+            </div>
+            <div className="flex flex-col space-y-1.5">
+              <Label htmlFor="password">Password</Label>
+              <Input id="password" type="password" placeholder="Password" {...register("password")} />
+              {errors.password && (
+                <Label className="text-destructive">
+                  {errors.password?.message}
+                </Label>
+              )}
+            </div>
+            <Button type="submit">SignIn</Button>
+          </div>
+        </form>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default SignIn;
