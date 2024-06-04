03de35f6445ed45d477fba5e2b791999e8692dac
gr7me <lsinjali40@gmail.com>
2024-06-04 14:32:00 +0545

    feat: add dashboard layout

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,30 +1,53 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, Navigate, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  Navigate,
+  RouterProvider,
+} from "react-router-dom";
 import "./index.css";
 import SignUp from "@/pages/login/SignUp.tsx";
 import SignIn from "@/pages/login/SignIn.tsx";
 import LoginLayout from "./layout/LoginLayout";
+import DashboardLayout from "@/layout/DashboardLayout";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Navigate to="/auth/login"/>,
+    element: <Navigate to="/auth/login" />,
   },
   {
     path: "/auth",
-    element: <LoginLayout/>,
+    element: <LoginLayout />,
     children: [
       {
         path: "login/",
-        element: <SignIn/>
+        element: <SignIn />,
       },
       {
         path: "signup/",
         element: <SignUp />,
       },
-    ]
-  }
+    ],
+  },
+  {
+    path: "/dashboard",
+    element: <DashboardLayout />,
+    children: [
+      {
+        index: true,
+        element: <p>Dashboard page</p>,
+      },
+      {
+        path: "logs/",
+        element: <p>logs page</p>,
+      },
+      {
+        path: "settings/",
+        element: <p>settings page</p>,
+      },
+    ],
+  },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
