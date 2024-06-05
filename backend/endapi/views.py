639ed799456a046d4273a5a2cc653f43bb10f86c
Arun Bhandari <abbhan053@gmail.com>
2024-06-05 14:27:28 +0545

    Fix:error

--- backend/endapi/views.py
+++ backend/endapi/views.py
@@ -1,22 +1,24 @@
+from rest_framework.decorators import api_view
 from django.http import HttpResponse
-from .serializers import AdminsSerializer,SigninSerializer
+from .serializers import AdminsSerializer, SigninSerializer
 from rest_framework.renderers import JSONRenderer
-from .models import Admins,Signin
+from .models import Admins, Signin
 
 # Create your views here.
 # views.py
 
+
+@api_view(["GET"])
 def signin_view(request):
     signin = Signin.objects.all()
     serializer = SigninSerializer(signin, many=True)
     serialized_data = JSONRenderer().render(serializer.data)
-    return HttpResponse(serialized_data, content_type='application/json')
+    return HttpResponse(serialized_data, content_type="application/json")
 
 
+@api_view(["GET"])
 def admins_view(request):
     admins = Admins.objects.all()
     serializer = AdminsSerializer(admins, many=True)
     serialized_data = JSONRenderer().render(serializer.data)
-    return HttpResponse(serialized_data, content_type='application/json') 
-
-
+    return HttpResponse(serialized_data, content_type="application/json")
