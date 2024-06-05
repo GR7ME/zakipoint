e5739250283863824b141e77244606b21f144066
gr7me <lsinjali40@gmail.com>
2024-06-05 16:00:16 +0545

    chore: routes

--- frontend/src/layout/LoginLayout.tsx
+++ frontend/src/layout/LoginLayout.tsx
@@ -3,7 +3,7 @@ import zph from "../assets/zph.webp"
 
 const LoginLayout = ()=>{
     return <>
-    <div className="w-[100%] h-[100%] flex flex-col gap-4 justify-center items-center">
+    <div className="w-full h-full flex flex-col gap-4 justify-center items-center">
     <img src={zph} className="h-24"></img>
     <Outlet/>
     </div>
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,59 +1,59 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import {
-  createBrowserRouter,
-  Navigate,
-  RouterProvider,
-} from "react-router-dom";
-import "./index.css";
-import SignUp from "@/pages/login/SignUp.tsx";
-import SignIn from "@/pages/login/SignIn.tsx";
-import LoginLayout from "./layout/LoginLayout";
-import DashboardLayout from "@/layout/DashboardLayout";
-import { Toaster } from "@/components/ui/sonner"
+// import {
+//   createBrowserRouter,
+//   Navigate,
+//   RouterProvider,
+// } from "react-router-dom";
+// import "./index.css";
+// import SignUp from "@/pages/login/SignUp.tsx";
+// import SignIn from "@/pages/login/SignIn.tsx";
+// import LoginLayout from "./layout/LoginLayout";
+// import DashboardLayout from "@/layout/DashboardLayout";
+// import { Toaster } from "@/components/ui/sonner"
+import App from "./App";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Navigate to="/auth/login" />,
-  },
-  {
-    path: "/auth",
-    element: <LoginLayout />,
-    children: [
-      {
-        path: "login/",
-        element: <SignIn />,
-      },
-      {
-        path: "signup/",
-        element: <SignUp />,
-      },
-    ],
-  },
-  {
-    path: "/dashboard",
-    element: <DashboardLayout />,
-    children: [
-      {
-        index: true,
-        element: <p>Dashboard page</p>,
-      },
-      {
-        path: "logs/",
-        element: <p>logs page</p>,
-      },
-      {
-        path: "settings/",
-        element: <p>settings page</p>,
-      },
-    ],
-  },
-]);
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <Navigate to="/auth/login" />,
+//   },
+//   {
+//     path: "/auth",
+//     element: <LoginLayout />,
+//     children: [
+//       {
+//         path: "login/",
+//         element: <SignIn />,
+//       },
+//       {
+//         path: "signup/",
+//         element: <SignUp />,
+//       },
+//     ],
+//   },
+//   {
+//     path: "/dashboard",
+//     element: <DashboardLayout />,
+//     children: [
+//       {
+//         index: true,
+//         element: <p>Dashboard page</p>,
+//       },
+//       {
+//         path: "logs/",
+//         element: <p>logs page</p>,
+//       },
+//       {
+//         path: "settings/",
+//         element: <p>settings page</p>,
+//       },
+//     ],
+//   },
+// ]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
-    <Toaster richColors position="top-right" />
+    <App/>
   </React.StrictMode>,
 );
